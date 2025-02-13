44e3a0f3cd495bc4541bd18c27d7592ba25e20f0
Slate Colebank <colebas@wwu.edu>
2025-02-13 00:16:32 -0800

    format cves to readable format

--- chromadb_test/newCreateDB.py
+++ chromadb_test/newCreateDB.py
@@ -1,5 +1,9 @@
+import os
 import json
 import chromadb
+
+# The amount of CVEs to put in the DB, -1 for no limit
+limit = 100
 
 cve_dir = "./CVEdata/cvelistnosub/cves/"
 
@@ -9,30 +13,55 @@ collection = client.get_or_create_collection("cve_collection")
 resources = []
 
 def process_cve_file(filename):
-    f = open(cve_dir + filename)
-    cve_data = json.load(f)
+    if filename.startswith("CVE"):
+        f = open(cve_dir + filename)
+        cve_data = json.load(f)
 
-    id = cve_data['cveMetadata']['cveId']
-    metadata = cve_data['cveMetadata']
-    affected_product = cve_data['containers']['cna']['affected'][0]['product']
-    description = cve_data['containers']['cna']['descriptions'][0]['value']
+        #id = cve_data['cveMetadata']['cveId']
+        #metadata = cve_data['cveMetadata']
+        #affected_product = cve_data['containers']['cna']['affected'][0]['product']
+        #description = cve_data['containers']['cna']['descriptions'][0]['value']
 
-    cve = {
-        "id": id,
-        "metadata": {
-            "assigner_name": metadata['assignerShortName'],
-            "date_published": metadata['datePublished'],
-            "affected_product": affected_product
-        },
-        "document": description
-    }
 
-    if metadata and affected_product and description:
+        id = cve_data.get('cveMetadata', {}).get('cveId', 'Unknown ID')
+
+        # Extract Description (first available description)
+        description = (
+            cve_data.get('containers', {})
+                    .get('cna', {})
+                    .get('descriptions', [{}])[0]
+                    .get('value', 'No Description Available')
+        )
+
+        # Extract Affected Products (list of products)
+        affected_products = [
+            entry.get('product', 'Unknown Product')
+            for entry in cve_data.get('containers', {}).get('cna', {}).get('affected', [])
+        ]
+
+
+        cve = {
+            "id": id,
+            "metadata": {
+                "affected_products": ', '.join(affected_products)
+            },
+            "document": description
+        }
+
         resources.append(cve)
 
+count = 0
+for filename in os.listdir(cve_dir):
+    print("adding file " + str(count))
+    process_cve_file(filename)
+    count += 1
+    if count > limit:
+        break;
 
-cve_name = "CVE-2024-25316.json"
-process_cve_file(cve_name)
+
+# force one of the libre office cves to be added
+libre_cve_name = "CVE-2023-2255.json"
+process_cve_file(libre_cve_name)
 
 collection.add(
     ids=[entry['id'] for entry in resources],
--- chromadb_test/newQueryLLM.py
+++ chromadb_test/newQueryLLM.py
@@ -1,10 +1,20 @@
+import json
 import ollama
 import newQueryDB
-import json
+import UserData
 
 
 cve_context = newQueryDB.get_cve_context()
-query = "What is the CVE that was provided to you?\nList of CVEs:\n" + json.dumps(cve_context)
+user_data = UserData.get_user_data()
+query = f"""
+The following is a list of upgradeable packages on a system:
+{user_data}
+Using the following list of CVEs, are there any vulnerabilities in any of the upgradeable packages? If there are, cite which CVE the vulnerability arises from.
+List of possibly related CVEs:
+{cve_context}
+"""
+
+# print(query)
 
 response = ollama.chat(
     model = "forced-cve",
@@ -16,4 +26,4 @@ response = ollama.chat(
     ]
 )
 
-print("Response:\n" + response)
+print(response["message"]["content"])
